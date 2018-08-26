05fcea06741401b71794275beef8bf122cae2253
Adam Geoffrion <adam@redivus.com>
2018-08-26 10:47:33 -0500

    Added decision display options

--- src/components/Decision.js
+++ src/components/Decision.js
@@ -1,20 +1,81 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { setCurrentTree, addSelectedTree, deleteSelectedTree } from '../redux/app'
 
 class Decision extends React.Component {
+    getParentNode(id) {
+        const { trees } = this.props
+        let parent = ''
+        trees.map(t => {
+            const checkChildren = (node) => {
+                node.nodes.map(n => {
+                    if (n.id == id) parent = node.id
+                    else checkChildren(n)
+                })
+            }
+            checkChildren(t)
+        })
+        console.log('Parent id: ' + parent)
+        return parent
+    }
+    renderNodes(node) {
+        const { setCurrentTree } = this.props
+        return (
+            <div>
+                <p onClick={()=>setCurrentTree(this.getParentNode(node.id))}>{node.name}</p>
+                {node.nodes.map(n => (
+                    <p onClick={()=>setCurrentTree(n.id)}>{n.name}</p>
+                ))}
+            </div>
+        )
+    }
+
+    renderCurrentTree() {
+        const { trees, currentTree, setCurrentTree } = this.props
+        if (currentTree === '') {
+            return trees.map(t => (<p onClick={()=>setCurrentTree(t.id)}>{t.name}</p>))
+        }
+        else {
+            let node = null
+            const checkNodes = (n) => {
+                if (n.id === currentTree) return node = n
+                else n.nodes.map(j => checkNodes(j))
+            }
+            trees.map(t => {
+                checkNodes(t)
+            })
+            return this.renderNodes(node)
+        }
+    }
+
 	render() {
-		
-        return (<p>Decision Page</p>)
+        const { trees, currentTree } = this.props
+        return (
+            <div>
+                <h1>{'Pick entries'}</h1>
+                {this.renderCurrentTree()}
+            </div>
+        )
 	}
 }
 Decision.propTypes = {
-	
+    trees: PropTypes.array.isRequired,
+    currentTree: PropTypes.string.isRequired,
+    selectedTrees: PropTypes.array.isRequired,
+    //Fuctions
+    setCurrentTree: PropTypes.func.isRequired,
+    addSelectedTree: PropTypes.func.isRequired,
+    deleteSelectedTree: PropTypes.func.isRequired
 }
 const mapStateToProps = state => ({
-	
+	trees: state.tree.data,
+    currentTree: state.app.currentTree,
+    selectedTrees: state.app.selectedTrees
 })
 const mapDispatchToProps = dispatch => ({
-	
+	setCurrentTree: (id) => dispatch(setCurrentTree(id)),
+    addSelectedTree: (id) => dispatch(addSelectedTree(id)),
+    deleteSelectedTree: (id) => dispatch(deleteSelectedTree(id))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Decision)
--- src/redux/app.js
+++ src/redux/app.js
@@ -3,14 +3,40 @@ import Immutable from 'seamless-immutable'
 import { aViewManage } from '../lib/constants'
 
 let INITIAL_STATE = Immutable({
-    adminView: aViewManage
+    adminView: aViewManage,
+		currentTree: '',
+		selectedTrees: []
 })
 
 // reducer
 export default handleActions({
 	RESET_USER_STATE: () => (INITIAL_STATE),
 	REHYDRATE_ALL_STATES: (state) => ({...state, LoadedFromDisk: true}),
-	SET_ADMIN_VIEW: (state, action) => ({...state, adminView: action.payload})
+	SET_ADMIN_VIEW: (state, action) => ({...state, adminView: action.payload}),
+	SET_CURRENT_TREE: (state, action) => ({...state, currentTree: action.payload}),
+	SET_SELECTED_TREES: (state, action) => ({...state, setSelectedTree: action.payload})
 }, INITIAL_STATE)
 
 export const setAdminView = createAction('SET_ADMIN_VIEW')
+export const setCurrentTree = createAction('SET_CURRENT_TREE')
+export const setSelectedTrees = createAction('SET_SELECTED_TREES')
+export const addSelectedTree = (id) => {
+	return (dispatch, getState) => {
+		let data = [...getState().app.selectedTrees]
+		dispatch(setSelectedTrees([...data, id]))
+	}
+}
+export const deleteSelectedTree = (id) => {
+	return (dispatch, getState) => {
+		let data = [...getState().app.selectedTrees]
+		let index = -1
+		data.every((d, i) => {
+			if (d === id) {
+				index = i
+				return false
+			}
+			return true
+		})
+		dispatch(setSelectedTrees(data.splice(index, 1)))
+	}
+}
